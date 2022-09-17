a29b2705f4a194f3d016fc365eedda320053296d
ValeriaRuizAreas <ruizareasvaleria@gmail.com>
2022-09-16 21:29:17 -0300

    BaSP-W05-2022: First and second exercises done

---
+++ Week-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,21 @@
+console.log('Exercise 01: Variables and Operators');
+// a. Create two numeric variables
+var dogFoodCost, catFoodCost, totalCost;
+dogFoodCost = 4000;
+catFoodCost = 2000;
+totalCost = dogFoodCost + catFoodCost;
+console.log('Total cost of pets food is', totalCost);
+
+// b. Create two string variables
+var myDogName, myCatName, myPetsName;
+myDogName = 'Mia Josefina ';
+myCatName = 'Gatastrofica';
+myPetsName = myDogName + myCatName;
+console.log('My pets name are', myPetsName);
+
+// c. Create two string variables (use length)
+var myFavoriteWine, myFavoriteBeer, characterLength;
+myFavoriteWine = 'Preciado';
+myFavoriteBeer = 'Toña';
+characterLength = myFavoriteWine.length + myFavoriteBeer.length;
+console.log('The ammount of character is', characterLength);
---
+++ Week-05/exercises/02_strings.js
@@ -0,0 +1,37 @@
+console.log('Exercise 02: Strings');
+// a. Create a string variable 
+var city = 'Montevideo';
+city = city.toUpperCase();
+console.log('I live in ', city);
+
+// b. Create a substring variable with the first 5 character
+var anotherCity, shortCity;
+anotherCity = 'Chinandega';
+shortCity = anotherCity.substring(0, 4);
+console.log('The short city name is ', shortCity);
+
+// c. Create a substring variable with the last 3 character
+var myCountry, shortCountry;
+myCountry = 'My beloved Nicaragua';
+shortCountry = myCountry.substring(17);
+console.log('The last three letters of Nicaragua are ', shortCountry);
+
+// d. Create a variable using substring, toUpperCase, toLowerCase and +
+var myComputer, myComputerCapitalized;
+myComputer = 'my Dell Laptop';
+myComputerCapitalized = myComputer.substring(0, 1).toUpperCase() + myComputer.substring(1).toLowerCase();
+console.log(myComputerCapitalized);
+
+// e. Create a variable finding the first space using indexOf
+var wordWithSpaces, firstSpace;
+wordWithSpaces = 'My favorite movie is'
+firstSpace = wordWithSpaces.indexOf(' ');
+console.log('The first space of',wordWithSpaces,'is in the position', firstSpace);
+
+// f. Create a variable using indexOf, substring, toUpperCase, toLowerCase and +
+var animalNames, result, space;
+animalNames = 'hUMMINGBIRD hipPopotamus';
+space = animalNames.indexOf(' ');
+result = animalNames.substring(0, 1).toUpperCase() + animalNames.substring(1, space).toLowerCase();
+result += animalNames.substring(space + 1, space + 2).toUpperCase() + animalNames.substring(space + 2).toLowerCase();
+console.log('The result is ', result);
